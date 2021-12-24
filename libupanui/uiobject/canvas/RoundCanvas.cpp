362efc732ea34138461840ab9cbfbc66cd62883d
Prajwala Prabhakar <srinivasa.prajwal@gmail.com>
2021-12-24 22:39:58 +0800

    draw anti-aliased circle/round canvas

--- libupanui/uiobject/canvas/RoundCanvas.cpp
+++ libupanui/uiobject/canvas/RoundCanvas.cpp
@@ -23,12 +23,13 @@
 #include <RoundCanvas.h>
 #include <GraphicsContext.h>
 #include "GCoreFunctions.h"
+#include <math.h>
 
 namespace upanui {
   RoundCanvas::RoundCanvas(const int x, const int y, const uint32_t width, const uint32_t height) : Canvas(x, y, width, height) {
   }
 
-  static void plot(int x, int y, int r, const FrameBuffer& framebuffer, uint32_t color) {
+  static void plotLine(int x, int y, int r, const FrameBuffer& framebuffer, uint32_t color) {
     int sx1 = -x + r;
     int sx2 = x + r;
     int sy = r - y;
@@ -58,6 +59,32 @@ namespace upanui {
     }
   }
 
+  static void plotPixel(int x, int y, int r, const FrameBuffer& framebuffer, uint32_t color) {
+    int sx1 = -x + r;
+    int sx2 = x + r;
+    int sy = r - y;
+    int yoffset = sy * framebuffer.width();
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx1 + yoffset], color);
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx2 + yoffset], color);
+
+    sy = r + y;
+    yoffset = sy * framebuffer.width();
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx1 + yoffset], color);
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx2 + yoffset], color);
+
+    sy = x + r;
+    sx1 = r - y;
+    sx2 = r + y;
+    yoffset = sy * framebuffer.width();
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx1 + yoffset], color);
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx2 + yoffset], color);
+
+    sy = -x + r;
+    yoffset = sy * framebuffer.width();
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx1 + yoffset], color);
+    GCoreFunctions::setPixel(framebuffer.buffer()[sx2 + yoffset], color);
+  }
+
   void RoundCanvas::fill() {
     const auto alpha = backgroundColorAlpha();
     if (alpha == 0) {
@@ -65,23 +92,36 @@ namespace upanui {
     }
 
     const auto& framebuffer = drawBuffer();
-    const auto color = (backgroundColorForDraw() & ~GCoreFunctions::ALPHA_MASK) | (alpha << 24);
+    const auto rawColor = (backgroundColorForDraw() & ~GCoreFunctions::ALPHA_MASK);
+    const auto color = rawColor | (alpha << 24);
 
     const int r = width() / 2;
     int x = 0;
     int y = r;
     int d = 3 - 2 * r;
+    const int r2 = r * r;
 
     while(x <= y) {
-      plot(x, y, r, framebuffer, color);
+      plotLine(x, y, r, framebuffer, color);
 
       if (d < 0) {
         d += 4 * x + 6;
-        ++x;
       } else {
         d += 4 * (x - y) + 10;
-        ++x;
-        --y;
+        //we are going to calculate actual y as part of anti-aliasing code below
+        //--y;
+      }
+      ++x;
+
+      float ya = sqrt(r2 - x * x);
+      //because the value of y is positive for the calculated quadrant, it's ok to truncate ya instead of floor;
+      //y = floor(ya);
+      y = ya;
+      auto e = ya - y;
+
+      if (e > 0 && e < 1) {
+        const auto antialiasPixelColor = rawColor | ((int) (alpha * e)) << 24;
+        plotPixel(x, y + 1, r, framebuffer, antialiasPixelColor);
       }
     }
   }
