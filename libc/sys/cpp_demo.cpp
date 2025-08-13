66fd5f63c9367f25fe03dd67e77790ce3d8486af
Prajwala Prabhakar <srinivasa.prajwal@gmail.com>
2025-08-13 22:25:23 +0800

    1. cross compiler rebuilt with configuration changes to generate crtbeginS.o and crtendS.o 2. Modified upanlibs build configuration to use a linker script and the necessary crt.* and crtbeginS.o and crtendS.o files for all shared libraries including libc.so 3. (2) is done to ensure that the necessary C++ runtime handlers for rtti, exceptions and global object construction and destruction are compiled along with - so, that it is possible to fuse C++ code inside C shared library. This will allow us to implement several standard (posix) libc functions using C++ code behind the scenes 4. process initialization code is triggered via _process_init_relocate - which relocates all dynamic symbols acros exe and loaded shared libraries and returns the list of _init and _fini function pointers for exe and all the loaded shared libraries - which will then be called inside the libc implementation of _process_init_reloacte 5. Provided implementation for __cxa_atexit() and __cxa_finalize() - to ensure the destructors of globally initialized and function static objects are called upon process exit

---
+++ libc/sys/cpp_demo.cpp
@@ -0,0 +1,48 @@
+/*
+ *	Upanix - An x86 based Operating System
+ *  Copyright (C) 2011 'Prajwala Prabhakar' 'srinivasa.prajwal@gmail.com'
+ *
+ *  I am making my contributions/submissions to this project solely in
+ *  my personal capacity and am not conveying any rights to any
+ *  intellectual property of any third parties.
+ *                                                                          
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *                                                                          
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *                                                                          
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#include <cpp_demo.h>
+#include <vector.h>
+
+class CppDemo {
+public:
+  CppDemo() {
+    printf("\nCPP DEMO Class Constructor");
+  }
+  ~CppDemo() {
+    printf("\nCPP DEMO Class Destructor");
+  }
+
+  void call_throw() {
+    throw 999;
+  }
+};
+
+static CppDemo cpp_demo1;
+
+void init_cpp_demo() {
+  try {
+    cpp_demo1.call_throw();
+  } catch(int e) {
+    printf("\n Exception Caught: %d", e);
+  }
+}
